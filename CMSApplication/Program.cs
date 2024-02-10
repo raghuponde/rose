d9286a2f86b17832b834d139530de979b4810b2c
neojarvis <techadmin@iamneo.ai>
2024-02-10 10:29:42 +0000

    Auto-save-commit-10/02/2024--15:59:40

--- CMSApplication/Program.cs
+++ CMSApplication/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+namespace CMSApplication
+{
+
+
+  public class Program
+  {
+    static List<Student> studlist = new List<Student>();
+    public static void Main(string[] args)
+    {
+      studlist.Add(new Student { Id = 101, Name = "Kirankumar"});
+      studlist.Add(new Student { Id = 102, Name = "Suresh" });
+      studlist.Add(new Student { Id = 101, Name = "Mohan" });
+
+
+      foreach (Student st in studlist)
+      {
+        Console.WriteLine("{0}--{1}", st.Id, st.Name);
+      }
+
+
+    }
+
+
+
+  }
+}
+
+
---
+++ CMSApplication/Student.cs
@@ -0,0 +1,10 @@
+namespace CMSApplication
+{
+    public class Student
+    {
+        public int Id {set;get;}
+
+        public string Name {set;get;}
+    }    
+    
+}
